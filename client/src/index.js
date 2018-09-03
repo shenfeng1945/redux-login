12a399412113861e238cbdb351daf5c8a7643fb3
shenfeng1945 <823902826@qq.com>
2018-09-03 10:49:13 +0800

    add redux and plugin

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import {logger} from 'redux-logger'
+import {composeWithDevTools} from 'redux-devtools-extension'
+import {createStore,applyMiddleware} from 'redux'
+import thunk from 'redux-thunk'
+import {Provider} from 'react-redux'
+import rootReducer from './reducers'
+const store = createStore(
+    rootReducer,
+    composeWithDevTools(
+     applyMiddleware(thunk,logger)
+    )
+)
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+<Provider store={store}>
+  <App />
+</Provider>,
+  document.getElementById('root'));
 registerServiceWorker();
