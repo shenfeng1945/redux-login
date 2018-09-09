e57f8e1a1c6c10fc1ad9743b0245ce813428f062
shenfeng1945 <823902826@qq.com>
2018-09-09 23:31:34 +0800

    后台验证数据为空的错误返回。

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,34 @@
 import express from 'express';
+import isEmpty from 'lodash/isEmpty';
 
 let router = express.Router();
 
+const validtorInput = (data) =>{
+   const errors = {}; 
+   // 四个为空的判断
+   if(data.username === ''){
+       errors.username = 'The field is required';
+   }
+   if(data.email === ''){
+       errors.email = 'The field is required';
+   }
+   if(data.password === ''){
+       errors.password = 'The field is required';
+   }
+   if(data.conPassword === ''){
+       errors.conPassword = 'The field is required';
+   }
+   return {
+       errors,
+       isValid: isEmpty(errors)
+   }
+}
+
 router.post('/',(req,res)=>{
-   console.log(req.body)
+   const {errors,isValid} = validtorInput(req.body);
+   if(!isValid){
+       res.status(404).json(errors)
+   }
 })
 
 export default router;
