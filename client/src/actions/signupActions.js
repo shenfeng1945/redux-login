cbcbdbc40d6fe4bf9cd1d362fbd3dd341884d269
shenfeng1945 <823902826@qq.com>
2018-09-23 11:16:15 +0800

    前端注册验证，是否用户名或邮箱是否存在

--- client/src/actions/signupActions.js
+++ client/src/actions/signupActions.js
@@ -1,6 +1,12 @@
 import axios from 'axios';
 export const userSignup = (userData) => {
     return dispatch => {
-        return axios.post('/api/users/signup',userData)
+        return axios.post('/api/users/signup', userData)
     }
 }
+export const userIdentify = (identify) => {
+    return dispatch => {
+        return axios.get(`/api/users/${identify}`)
+    }
+}
+
