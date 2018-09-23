f8ab4f4c274002a86f935d4a68585736641aa44a
shenfeng1945 <823902826@qq.com>
2018-09-23 11:18:05 +0800

    配合注册页的前端验证

--- server/routes/users.js
+++ server/routes/users.js
@@ -4,6 +4,9 @@ import validator from 'validator';
 import mongoose from 'mongoose';
 import User from '../models/user';
 import bcrypt from 'bcrypt'; //加密密码
+import jwt from 'jsonwebtoken';
+import config from '../config'
+import Authorization from '../middleware/judgeAuthorization'
 let router = express.Router();
 
 const validtorInput = (data) => {
@@ -69,22 +72,43 @@ router.post('/signup', (req, res, next) => {
         res.status(400).json(errors);
     }
 })
+// 注册前端验证,失去焦点，验证用户名或邮箱是否存在
+router.get('/:identify',(req,res)=>{
+    const {identify} = req.params;
+    User.find({$or: [{username: identify},{email: identify}]},(err_docs,docs)=>{
+        if(docs.length){
+           res.status(200).json({identify:true})
+        }else{
+           res.status(200).json({identify:false})
+        }
+    })
+})
 
 router.post('/login', (req, res, next) => {
     const { users, password } = req.body;
     const errors = {};
     User.find({ $or: [{ username: users }, { email: users }] }, (err_docs, docs) => {
-        if (docs.length === 1) {
+        if (docs.length) { // 找到了一个
             bcrypt.compare(password, docs[0].password, (err_b, res_b) => {
                 if (res_b) {
-                    res.status(200).json({ success: '登录成功' })
+                    const token = jwt.sign({
+                        id:docs[0]._id,
+                        username: docs[0].username
+                    },config.jwtSecret)
+                    res.status(200).json({token})
                 } else {
                     errors.password = '密码不正确'
-                    res.status(400).json(errors)
+                    res.status(400).json({errors})
                 }
             })
+        }else{
+           errors.users = '用户名或邮箱不存在'
+           res.status(400).json({errors})
         }
     })
 })
 
+router.post('/create',Authorization,(req,res)=>{
+    res.status(200).json({user: req.user})
+})
 export default router;
